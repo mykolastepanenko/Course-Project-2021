a6ab92ae8ac1ed1af2ce07796cfc8c9b6a47b930
unknown <Nikolaua36@gmail.com>
2021-01-23 16:48:25 +0200

    player control

--- Stepanenko MI Course Project 2021/Form1.Designer.cs
+++ Stepanenko MI Course Project 2021/Form1.Designer.cs
@@ -30,15 +30,9 @@ namespace Stepanenko_MI_Course_Project_2021
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            for (int i = 0; i < myBtn.Length; i++)
-            {
-                this.myBtn[i] = new System.Windows.Forms.Button[30];
-                for (int j = 0; j < myBtn.Length; j++)
-                {
-                    this.myBtn[i][j] = new System.Windows.Forms.Button();
-                }
-            }
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -47,48 +41,43 @@ namespace Stepanenko_MI_Course_Project_2021
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(18, 18);
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1402, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(974, 24);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(950, 432);
+            this.panel1.TabIndex = 3;
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(479, 125);
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(775, 392);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(86, 26);
-            this.button1.TabIndex = 3;
+            this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            //
-            // myBtn
-            //
-            for (int i = 0; i < myBtn.Length; i++)
-            {
-                for (int j = 0; j < myBtn.Length; j++)
-                {
-                    this.myBtn[i][j].Location = new System.Drawing.Point(i * 25, (j+1) * 25);
-                    this.myBtn[i][j].Name = "arr" + i + j;
-                    this.myBtn[i][j].Size = new System.Drawing.Size(25, 25);
-                }
-            }
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1402, 691);
-            this.Controls.Add(this.button1);
-            for (int i = 0; i < myBtn.Length; i++)
-            {
-                for (int j = 0; j < myBtn.Length; j++)
-                {
-                    this.Controls.Add(this.myBtn[i][j]);
-                }
-            }
+            this.ClientSize = new System.Drawing.Size(974, 488);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,8 +85,10 @@ namespace Stepanenko_MI_Course_Project_2021
 
         #endregion
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button[][] field = new System.Windows.Forms.Button[10][];
+        private System.Windows.Forms.Button player = new System.Windows.Forms.Button();
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button[][] myBtn = new System.Windows.Forms.Button[30][];
     }
 }
 
--- Stepanenko MI Course Project 2021/Form1.cs
+++ Stepanenko MI Course Project 2021/Form1.cs
@@ -12,31 +12,78 @@ namespace Stepanenko_MI_Course_Project_2021
 {
     public partial class Form1 : Form
     {
+        int[] playerLocation = new int[2];
         public Form1()
         {
             InitializeComponent();
-            //Button[][] field = new Button[30][];
-
-            //for(int i = 0; i < field.Length; i++)
-            //{
-            //    field[i] = new Button[30];
-            //}
-            
-
-
-            //for(int i = 0; i < 2; i++)
-            //{
-            //    myBtn[i].Size = new System.Drawing.Size(25, 25);
-            //    myBtn[i].Location = new System.Drawing.Point(25 * i, 25 * i);
-            //    myBtn[i].Name = "MyButton" + i;
-            //    panel1.Controls.Add(myBtn[i]);
-            //}
-
-            //this.Controls.Add(myBtn[0]);
-
-            //this.Controls.Add(myBtn[0]);
-
+            InitializeField();
+        }
+        private void InitializeField()
+        {
+            for (int i = 0; i < field.Length; i++)
+            {
+                this.field[i] = new System.Windows.Forms.Button[30];
+                for (int j = 0; j < field.Length; j++)
+                {
+                    this.field[i][j] = new System.Windows.Forms.Button();
+                }
+            }
+            for (int i = 0; i < field.Length; i++)
+            {
+                for (int j = 0; j < field.Length; j++)
+                {
+                    this.field[i][j].Location = new System.Drawing.Point(i * 25, j * 25);
+                    this.field[i][j].Name = "arr" + i + j;
+                    this.field[i][j].Size = new System.Drawing.Size(25, 25);
+                }
+            }
+            InitializePlayer();
+            for (int i = 0; i < field.Length; i++)
+            {
+                for (int j = 0; j < field.Length; j++)
+                {
+                    this.panel1.Controls.Add(this.field[i][j]);
+                }
+            }
+        }
+        private void InitializePlayer()
+        {
+            player.BackColor = Color.Red;
+            player.Size = new System.Drawing.Size(25, 25);
+            player.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Controls.Add(player);
         }
 
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                if(player.Location.Y != 0)
+                {
+                    this.player.Location = new System.Drawing.Point(player.Location.X, player.Location.Y - 25);
+                }
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                if(player.Location.Y != field.Length * 25 - 25)
+                {
+                    this.player.Location = new System.Drawing.Point(player.Location.X, player.Location.Y + 25);
+                }
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                if(player.Location.X != 0)
+                {
+                    this.player.Location = new System.Drawing.Point(player.Location.X - 25, player.Location.Y);
+                }
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                if(player.Location.X != field.Length * 25 - 25)
+                {
+                    this.player.Location = new System.Drawing.Point(player.Location.X + 25, player.Location.Y);
+                }
+            }
+        }
     }
 }
